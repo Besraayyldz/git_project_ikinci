43e5b73db48e4f4f521429f2b1f906faa5678081
Besraayyldz <69383831+Besraayyldz@users.noreply.github.com>
2020-08-24 14:30:22 +0300

    Update Deneme.java

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("deneme 01");
 		System.out.println("deneme 02");
+		System.out.println("remote deneme 01");
 
 	}
 
